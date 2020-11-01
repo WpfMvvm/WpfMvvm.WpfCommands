c7eba3fc69128e7342c359411ffc20fac8a6c6dd
EldHasp <EldHasp.WpfMvvm@Gmail.com>
2020-11-01 18:58:02 +0300

    Первая заливка на NuGet пустого пакета

---
+++ WpfMvvm.WpfCommands/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("WpfMvvm.WpfCommands")]
+[assembly: AssemblyDescription("Реализации ICommand для использования в WPF Решениях")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EldHasp")]
+[assembly: AssemblyProduct("WpfMvvm.WpfCommands")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b14bc203-eaf5-4234-9522-9e8751eef76d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
